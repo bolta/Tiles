3c8566871fbee726d4e11642223ea7d5e2322e71
bolta <freshness.and.speed.2007@gmail.com>
2016-02-07 03:49:01 +0900

    コンソールアプリとして main を整備

--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,42 @@
 module Main where
 
-import Colour
-import ColourGen
-import Divider
-import Draw
-import Figure
-import GraphicsContext
-import Tiles
-
-import Control.Monad.State
+import Control.Monad.State (evalState)
+import System.Environment (getArgs)
+import System.Exit
+import System.IO
 import System.Random
 
-imgSize@(width, height) = (640, 480)
+import Colour
+import ColourGen
+import Figure hiding (size)
+import GraphicsContext
+import Options
+import Settings
+import Tiles
 
-colours =
---	alternate [coloursFromSeed 42, coloursFromSeed 100, coloursFromSeed 1]
-	coloursFromSeed 42
-	where
-		coloursFromSeed seed =
-			evalState (rgbRandomWalk (-0.025, 0.025) (Rgb 0.5 0.5 0.5))
-				(mkStdGen seed)
---colours = synmetric $ take (40 * 30) $ evalState (rgbRandomWalk (-0.05, 0.05) (Rgb 0.5 0.5 0.5)) (mkStdGen 42)
-{-
-figures = {-synmetric $-} compositeDivider
-	[{-synmetric .-} tbDivider (height `div` 10), synmetric . lrDivider (width `div` 10)]
-	$ Rect (0, 0) imgSize
--}
+import DividerEval
+
+main = do
+	args <- getArgs
+	s <- parseCommandLineToSettings args
+	case s of
+		Left error -> do
+			hPutStr stderr error
+			exitFailure
+		Right settings -> runWith settings
+
+runWith settings =
+	let imgSize = size settings
+	in drawTilesOnBitmapFile
+		imgSize
+		(outputFile settings)
+		(backColour settings) 
+		(map fromColour $ coloursFromSeed $ randomSeed settings)
+		(divider settings $ Rect (0, 0) imgSize)
 
 
--- figures = rlbtDivider (16, 16) $ Rect (0, 0) imgSize
+-- TODO •Ê‚Ì‚Æ‚±‚ë‚É‚â‚è‚½‚¢
+coloursFromSeed seed =
+	evalState (rgbRandomWalk (-0.025, 0.025) (Rgb 0.5 0.5 0.5))
+		(mkStdGen seed)
 
-rootDivider = compositeDivider
-	[lrtbDivider (96, 80), lrtbDivider (8, 8)]
-
-figures = rootDivider $ Rect (0, 0) imgSize
-
---main = makeBitmapFileFromProc width height "main.bmp" (Rgb 0 0 0)
---	$ drawTiles (map fromColour colours) figures
-main =
-	drawTilesOnBitmapFile imgSize "main.bmp" (Rgb 0 0 0)
-		(map fromColour colours) figures
